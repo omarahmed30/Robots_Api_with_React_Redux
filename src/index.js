cbf4e3728b7ef438cd5d4a03295fb5a10dde75f8
omarahmed103 <omariahmed103@gmail.com>
2018-08-28 22:33:39 -0400

    Search bar is working

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import SearchBox from './SearchBox';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-
 
 ReactDOM.render(
     <App />
